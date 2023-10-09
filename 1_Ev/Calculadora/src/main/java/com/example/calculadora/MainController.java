bbb7a4f9025d2d60b85722fcecc20abd7f77cc1d
Victorgonzalez-ces <victor.gonzalez@cesjuanpablosegundo.es>
2023-10-09 14:23:40 +0200

    update

---
+++ 1_Ev/Calculadora/src/main/java/com/example/calculadora/MainController.java
@@ -0,0 +1,25 @@
+package com.example.calculadora;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable, EventHandler<ActionEvent> {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    private void acciones(){}
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+    }
+}
