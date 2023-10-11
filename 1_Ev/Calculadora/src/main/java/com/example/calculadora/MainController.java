65f2a1e3e1ea8a184c74a18be02b5ffdb6778de8
Victorgonzalez-ces <victor.gonzalez@cesjuanpablosegundo.es>
2023-10-11 13:45:07 +0200

    update

--- 1_Ev/Calculadora/src/main/java/com/example/calculadora/MainController.java
+++ 1_Ev/Calculadora/src/main/java/com/example/calculadora/MainController.java
@@ -4,13 +4,65 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable, EventHandler<ActionEvent> {
 
+    @FXML
+    private GridPane panelGeneral;
+    @FXML
+    private Button boton1;
+
+    @FXML
+    private Button boton2;
+
+    @FXML
+    private Button boton3;
+
+    @FXML
+    private Button boton4;
+
+    @FXML
+    private Button boton5;
+
+    @FXML
+    private Button boton6;
+
+    @FXML
+    private Button boton7;
+
+    @FXML
+    private Button boton8;
+
+    @FXML
+    private Button boton9;
+
+    @FXML
+    private Button boton0;
+
+    @FXML
+    private Button botonMas;
+
+    @FXML
+    private Button botonMenos;
+
+    @FXML
+    private Button botonPor;
+
+    @FXML
+    private Button botonDividir;
+
+    @FXML
+    private Button botonIgual;
+
+    @FXML
+    private TextField textoCalculadora;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -20,6 +72,8 @@ public class MainController implements Initializable, EventHandler<ActionEvent> 
     private void acciones(){}
     @Override
     public void handle(ActionEvent actionEvent) {
-
+        if (actionEvent.getSource()==boton1 || actionEvent.getSource()==boton2){
+            textoCalculadora.setText(((Button) (actionEvent.getSource())).getText());
+        }
     }
 }
--- 1_Ev/botones/src/main/java/com/example/botones/MainController.java
+++ 1_Ev/botones/src/main/java/com/example/botones/MainController.java
@@ -1,5 +1,9 @@
 package com.example.botones;
 
+import javafx.beans.binding.Binding;
+import javafx.beans.binding.Bindings;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
@@ -66,31 +70,75 @@ public class MainController implements Initializable, EventHandler<ActionEvent> 
     @FXML
     private Button botonComprobar;
 
+    private ToggleGroup grupoToggle;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        instancias();
+        configurarGrupoToggle();
         acciones();
         personalizarBotones();
     }
 
-    private void acciones(){
+    private void configurarGrupoToggle() {
+        grupoToggle.getToggles().addAll(toggle2,toggle3);
+    }
 
+    private void acciones(){
+        toggle1.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean old, Boolean newValue) {
+                if (newValue){
+                    System.out.println("elemento seleccionado");
+                }else{
+                    System.out.println("elemento deseleccionado");
+                }
+            }
+        });
+
+        grupoToggle.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observableValue, Toggle toggle, Toggle t1) {
+                if (t1 != null){
+                    System.out.println("El botón pulsado es: "+((ToggleButton)t1).getText());
+                }
+            }
+        });
         botonNormal.setOnAction(this);
         botonImagen.setOnAction(this);
         botonURL.setOnAction(this);
         botonGeneral.setOnAction(this);
+        botonComprobar.setOnAction(this);
     }
 
     private void personalizarBotones(){
+        String[] toggleUrl = new String[]{"https://cdn-icons-png.flaticon.com/512/443/443045.png","https://i.pinimg.com/originals/1c/ed/ec/1cedece6e9acb40aa238147139ee6e9e.png"};
+        toggle1.setBackground(null);
+        toggle1.setBorder(null);
         botonImagen.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("375.png"))));
         botonImagen.setBackground(null);
         botonImagen.setBorder(null);
+        ImageView imageToggle = new ImageView();
+        toggle1.setGraphic(imageToggle);
+        imageToggle.imageProperty()
+                .bind(Bindings.when(toggle1.selectedProperty())
+                        .then(new Image(toggleUrl[1]))
+                        .otherwise(new Image(toggleUrl[0])));
 
 
     }
 
     @Override
     public void handle(ActionEvent actionEvent) {
-
+        if (actionEvent.getSource() == botonComprobar){
+            System.out.println("Elemento seleccionado: ");
+            System.out.println(((ToggleButton)grupoToggle.getSelectedToggle()).getText());
+        }else{
+            System.out.println("No hay seleccion en el grupo");
+        }
+    }
+    private void instancias(){
+        grupoToggle = new ToggleGroup();
     }
 }
 
