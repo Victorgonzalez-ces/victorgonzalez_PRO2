2af93536d065655574492ddc89d62b4b8eca0840
Victorgonzalez-ces <114072908+Victorgonzalez-ces@users.noreply.github.com>
2023-11-11 12:08:55 +0100

    update

--- PSP_1Proyecto/src/main/kotlin/Hijo.kt
+++ PSP_1Proyecto/src/main/kotlin/Hijo.kt
@@ -1,26 +1,15 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun main() {
-    // Establecemos la comunicación con el proceso padre
-    val entradaPadre = BufferedReader(InputStreamReader(System.`in`))
-    val salidaPadre = OutputStreamWriter(System.out)
+    val inputReader = BufferedReader(InputStreamReader(System.`in`))
+    val outputWriter = OutputStreamWriter(System.out)
 
-    // Recibimos la pregunta del proceso padre
-    val pregunta = entradaPadre.readLine()
-    println("Proceso Hijo: Recibí la pregunta \"$pregunta\"")
+    val parentMessage = inputReader.readLine()
 
-    // Simulamos buscar el autor y sus 5 libros
-    val autor = "Autor Ejemplo"
-    val libros = listOf("Libro 1", "Libro 2", "Libro 3", "Libro 4", "Libro 5")
-
-    // Enviamos la respuesta al proceso padre
-    salidaPadre.write("$autor\n")
-    salidaPadre.write(libros.joinToString("\n"))
-    salidaPadre.flush()
-
-    // Cerramos la comunicación
-    salidaPadre.close()
-    entradaPadre.close()
+    outputWriter.write("$parentMessage\n")
+    outputWriter.flush()
 }
--- PSP_1Proyecto/src/main/kotlin/Padre.kt
+++ PSP_1Proyecto/src/main/kotlin/Padre.kt
@@ -1,22 +1,28 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.concurrent.thread
 
 fun main() {
-    val procesoPadre = ProcessBuilder("java", "-jar", "ProcesoPadre.jar").start()
-    val entradaPadre = BufferedReader(InputStreamReader(procesoPadre.inputStream))
-    val salidaPadre = OutputStreamWriter(procesoPadre.outputStream)
+    val processBuilder = ProcessBuilder("java","-cp","C:\\Program Files\\Java\\jdk-16.0.1\\bin\\java.exe\" \"-javaagent:C:\\Program Files\\JetBrains\\IntelliJ IDEA Community Edition 2022.2.3\\lib\\idea_rt.jar=51385:C:\\Program Files\\JetBrains\\IntelliJ IDEA Community Edition 2022.2.3\\bin\" -Dfile.encoding=UTF-8 -classpath C:\\Users\\victor\\Documents\\GitHub\\victorgonzalez_PRO2\\PSP_1Proyecto\\out\\production\\PSP_1Proyecto;C:\\Users\\victor\\.m2\\repository\\org\\jetbrains\\kotlin\\kotlin-stdlib-jdk8\\1.7.10\\kotlin-stdlib-jdk8-1.7.10.jar;C:\\Users\\victor\\.m2\\repository\\org\\jetbrains\\kotlin\\kotlin-stdlib\\1.7.10\\kotlin-stdlib-1.7.10.jar;C:\\Users\\victor\\.m2\\repository\\org\\jetbrains\\kotlin\\kotlin-stdlib-common\\1.7.10\\kotlin-stdlib-common-1.7.10.jar;C:\\Users\\victor\\.m2\\repository\\org\\jetbrains\\annotations\\13.0\\annotations-13.0.jar;C:\\Users\\victor\\.m2\\repository\\org\\jetbrains\\kotlin\\kotlin-stdlib-jdk7\\1.7.10\\kotlin-stdlib-jdk7-1.7.10.jar HijoKt")
+    processBuilder.redirectErrorStream(true)
+    val process = processBuilder.start()
 
-    println("Proceso Padre: ¿Quién es el autor de tu libro favorito?")
-    val libroFavorito = readLine()
+    val inputReader = BufferedReader(InputStreamReader(System.`in`))
+    val outputWriter = OutputStreamWriter(process.outputStream)
+    val inputChild = BufferedReader(InputStreamReader(process.inputStream))
 
-    salidaPadre.write(libroFavorito + "\n")
-    salidaPadre.flush()
+    val dateTime = LocalDateTime.now()
+        .format(DateTimeFormatter.ofPattern("dd MMM yyyy, hh:mm:ss a"))
+    val childMessage = "hoy es $dateTime"
 
-    val respuestaHijo = entradaPadre.readLine()
-    println("Proceso Padre: El autor de $libroFavorito ha escrito los siguientes 5 libros:")
-    println(respuestaHijo)
+    outputWriter.write("$childMessage\n")
+    outputWriter.flush()
 
-    salidaPadre.close()
-    entradaPadre.close()
+    val responseMessage = inputChild.readLine()
+    println("el mensaje del proceso hijo es: $responseMessage")
+
+    process.waitFor()
 }
