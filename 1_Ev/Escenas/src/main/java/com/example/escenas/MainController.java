10222841ed77256132c41ed1e47325db71efae1b
Victorgonzalez-ces <victor.gonzalez@cesjuanpablosegundo.es>
2023-11-21 10:16:20 +0100

    update

---
+++ 1_Ev/Escenas/src/main/java/com/example/escenas/MainController.java
@@ -0,0 +1,16 @@
+package com.example.escenas;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController implements Initializable{
+
+    @FXML
+    private VBox vBox;
+
+    @FXML
+    private TextField textFieldNombre;
+
+    @FXML
+    private Button botonComunicar;
+}
