6185e4c9e8ed61a08eaf90edf6fc824bef06125f
Victorgonzalez-ces <114072908+Victorgonzalez-ces@users.noreply.github.com>
2024-01-23 00:41:44 +0100

    update

--- 2Ev/T5/t5_Promesas/utils/js/ejercicio.js
+++ 2Ev/T5/t5_Promesas/utils/js/ejercicio.js
@@ -2,13 +2,16 @@ let categories = document.querySelector("#categorias");
 let cartas = document.querySelector("#cartas")
 let botonSearch = document.querySelector("#search")
 let inputProductos = document.querySelector("#numeroProductos")
+let listaProductos = document.querySelector("#listaProductos")
+let botonDetalle = document.querySelector("#verDetalle")
 let urlCategories = "https://dummyjson.com/products/categories"; 
-
 cargarCategorias()
 
 botonSearch.addEventListener("click", ()=>{
     cargarProductos(categories.value,inputProductos.value)
 })
+
+
 function cargarCategorias(){
     fetch(urlCategories)
     .then((res)=>{
@@ -37,14 +40,35 @@ function cargarProductos(categori,num){
             <p class='card-text'>${element.stock}</p> 
             <div class="row">
                 <button class="col btn btn-primary"
-                id="comprar-${element.id}">comprar</Button>
+                id="comprar">comprar</Button>
                 <button class="col btn btn-primary"
-                id=verDetalle-${element.id}
+                id=verDetalle
                 style="background-color: green"
                 >ver detalle</Button>
             </div>
             </div>
             </div>`
+            let botonComprar = document.querySelector(`#comprar`)
+            botonComprar.addEventListener("click",()=>{
+                anadirCarrito(element.id)
+            })
+        });
+    })
+    anadirCarrito();
+}
+
+function anadirCarrito(id){
+    url = "https://dummyjson.com/products";
+    fetch(url)
+    .then((res)=>{
+        return res.json();
+    })
+    .then((res1)=>{
+        res1.products.forEach(element => {
+            if(element.id == id){
+                listaProductos.innerHTML += `<li class='list-group-item' style='text-align: center'>
+                ${element.title}</li>`;
+            }
         });
     })
 }
