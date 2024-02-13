23bdac89cd0dde13548c27016b076aafda8bb9cc
Victorgonzalez-ces <114072908+Victorgonzalez-ces@users.noreply.github.com>
2024-02-13 17:45:39 +0100

    update

---
+++ 2Ev/T6/ProyectosAngular/repasoFinal/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ConcesionarioComponent } from './components/concesionario/concesionario.component';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConcesionarioComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 2Ev/T6/ProyectosAngular/repasoFinal/src/app/components/concesionario/concesionario.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { CocheClase } from '../../model/Coche';
+import Swal from 'sweetalert2';
+@Component({
+  selector: 'app-concesionario',
+  templateUrl: './concesionario.component.html',
+  styleUrl: './concesionario.component.css'
+})
+export class ConcesionarioComponent {
+    concesionario = "Concesionario SL";
+    imagenConcesionario = '';
+    marca: string = '';
+    modelo: string = '';
+    matricula: string = '';
+    cv: string = '';
+    cc: string = '';
+    motor: string = '';
+    precio: string = '';
+    listado: CocheClase[] = []
+
+
+    agregarCoche(){
+      if(
+        this.marca.length > 0 &&
+        this.modelo.length > 0 &&
+        this.matricula.length > 0 &&
+        Number(this.cc) > 0 &&
+        Number(this.cv) > 0 &&
+        Number(this.precio) > 0
+      ){
+        this.listado.push(new CocheClase(
+          this.marca,
+          this.modelo,
+          Number(this.cc),
+          Number(this.cv),
+          this.matricula,
+          Number(this.precio),
+          this.motor
+        ))
+
+        this.marca = '';
+        this.modelo = '';
+        this.cc = '';
+        this.cv = '';
+        this.matricula = '';
+        this.precio = '';
+        this.motor = '';
+
+        Swal.fire({
+          title: 'correcto',
+          text: 'Coche agregado correctamente',
+          icon: 'success',
+          confirmButtonText: 'Cool'
+        })
+      }else{
+        Swal.fire({
+          title: 'Error!',
+          text: 'Faltan datos',
+          icon: 'error',
+          confirmButtonText: 'Cool'
+        })
+      }
+    }
+}
